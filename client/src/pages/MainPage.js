304c3576787e0d0642f4e4491b4e56253b497cff
Harsha <kesrabandara1998@gmail.com>
2025-06-04 02:25:52 +0530

    create mainPage.js file and finished form part

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1 className="lg:mx-32  text-5xl font-black flex items-center justify-normal text-green-500">
+        Convert Your Currencies Today
+      </h1>
+      <p className="lg:mx-32 font-sm opacity-40 py-6">
+        Welcome to "Convert Your Currencies Today"! This application allows you
+        to easily convert currencies based on the latest exchange rates. Whether
+        you're planning a trip, managing your finances, or simply curious about
+        the value of your money in different currencies, this tool is here to
+        help.
+      </p>
+      <div className="mt-5 flex items-center justify-center flex-col">
+        <section className="w-full lg:w-1/2">
+          <form>
+            <div className="mb-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="date"
+              >
+                Date
+              </label>
+              <input
+                required
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                type="date"
+                name="date"
+                id="date"
+                placeholder="date.."
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor=""
+              >
+                Source Curency
+              </label>
+              <select
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                type="text"
+                name=""
+                id=""
+              >
+                <option value=""> select source currency</option>
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor=""
+              >
+                Target Curency
+              </label>
+              <select
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                type="text"
+                name=""
+                id=""
+              >
+                <option value=""> select Target currency</option>
+              </select>
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor=""
+              >
+                Amount in Source Curency
+              </label>
+              <input
+                required
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                type="text"
+                name="text"
+                id=""
+                placeholder="Enter amount"
+              />
+            </div>
+
+            <button className=" bg-green-600 hover:bg-green-700
+            text-white font-bold py-2 px-4 rounded"> Get the Target Curency</button>
+          </form>
+        </section>
+      </div>
+    </div>
+  );
+}

57d78a17af9d5011410eb670db06e7db843c4d33
Harsha <kesrabandara1998@gmail.com>
2025-06-04 21:22:19 +0530

    server start

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
-console.log('server is running..');
+const express = require(`express`);
+const cors = require(`cors`);
+const axios = require(`axios`);
+
+const app = express();
+
+//middle wares
+app.use(express.json());
+app.use(cors());
+
+app.listen(5000, () => {
+    console.log(`Server is running on port 5000`);
+});

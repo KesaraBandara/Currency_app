0c23e68d43b5e8c5af8f3c5b7a620b6b34d90e72
Harsha <kesrabandara1998@gmail.com>
2025-06-08 23:02:15 +0530

    create covert function

--- server/src/index.js
+++ server/src/index.js
@@ -9,20 +9,23 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/getAllCurrencies", async (req, res) => {
+  const namesURl = `https://openexchangerates.org/api/currencies.json?app_id=c7b988acb392475bb82f1182d2113334`;
 
-      const namesURl = `https://openexchangerates.org/api/currencies.json?app_id=c7b988acb392475bb82f1182d2113334`;
+  try {
+    const nameResponse = await axios.get(namesURl);
+    const nameData = nameResponse.data;
 
-    try {
-        const nameResponse = await axios.get(namesURl);
-        const nameData = nameResponse.data;
-        
-        return res.json(nameData);
+    return res.json(nameData);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-    } catch (err) {
-        console.log(err);
-    }
+app.get("/convert",async (req, res) => {
+  const { date, sourceCurrency, targetCurrency, amountInSourceCurrency } =
+    req.query;
 });
 
 app.listen(5000, () => {
-    console.log(`Server is running on port 5000`);
+  console.log(`Server is running on port 5000`);
 });
